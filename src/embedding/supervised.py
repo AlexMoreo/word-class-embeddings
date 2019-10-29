b128dff2b873cd9991cd97e36f256836b5ead94e
Alex Moreo <alejandro.moreo@isti.cnr.it>
2019-10-29 16:12:46 +0100

    zscore in front of pca

--- src/embedding/supervised.py
+++ src/embedding/supervised.py
@@ -56,6 +56,9 @@ def get_supervised_embeddings(X, Y, max_label_space=300, binary_structural_probl
     elif method == 'chi2':
         F = supervised_embeddings_tsr(X, Y, chi_square)
 
+    if dozscore:
+        F = zscores(F, axis=0)
+
     if nC > max_label_space:
         print(f'supervised matrix has more dimensions ({nC}) than the allowed limit {max_label_space}. '
               f'Applying PCA(n_components={max_label_space})')
@@ -63,8 +66,6 @@ def get_supervised_embeddings(X, Y, max_label_space=300, binary_structural_probl
         F = pca.fit(F).transform(F)
         F /= pca.singular_values_
 
-    if dozscore:
-        F = zscores(F, axis=0)
 
     return F
 
